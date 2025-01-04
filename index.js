3eced2fd18584b6252e9b5a82adc591194b61c28
RhysG123 <Flightsimmer162@gmail.com>
2025-01-04 10:40:12 +0000

    Rhys' Customs

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["👀 Watching Over Rhys' Customs", "🌟 Award Winning Design Server!"];
+const statusTypes = [ 'dnd', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
