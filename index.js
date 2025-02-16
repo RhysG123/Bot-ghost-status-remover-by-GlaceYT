11b394cd92b6ab6ab69d97d54d0676fe4a8098b7
RhysG123 <Flightsimmer162@gmail.com>
2025-02-16 18:32:28 +0000

    TUI

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Overseeing Liberty State Roleplay", "Hosted @ Rhys' Customs"];
+const statusMessages = ["Overseeing TUI Virtual", "Overseeing Tui Virtual"];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
